6d1a23a4f92193a52dd22ce1fae7c5d837f83a95
MitchellHodzen <MitchellHodzen@yahoo.com>
2026-05-02 21:23:47 -0400

    removed counter as it is unused

--- client/src/system_movement.c
+++ client/src/system_movement.c
@@ -5,7 +5,6 @@
 
 void s_move(struct ECDB const *const ec, int positions_handle, int inputs_handle, float deltaTimeS)
 {
-    int counter = 0;
     struct Vector2* positions = (struct Vector2*) ec->_componentArrays[positions_handle];
     struct C_Input* inputs = (struct C_Input*) ec->_componentArrays[inputs_handle];
     for(unsigned int i = 0; i < ec->_maxEntities; ++i)
@@ -14,7 +13,6 @@ void s_move(struct ECDB const *const ec, int positions_handle, int inputs_handle
         {
             positions[i].x += inputs[i].direction.x * inputs[i].speed * deltaTimeS;
             positions[i].y += inputs[i].direction.y * inputs[i].speed * deltaTimeS;
-            counter++;
         }
     }
 }
